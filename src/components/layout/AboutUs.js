e49c02e1ca8bde5a31e23826952c9241e49d1559
Abdulmuizz98 <hamzatabdulmuizz98@gmail.com>
2023-10-24 11:49:06 +0100

    feat: landing-page -> build responsive about and category section

---
+++ src/components/layout/AboutUs.js
@@ -0,0 +1,96 @@
+import necklaceTwo from "../../assets/necklace_2.png";
+import necklaceOne from "../../assets/necklace_1_mod.png";
+import earringsTwo from "../../assets/earrings_2.png";
+import ringsEarrings from "../../assets/rings_earrings.png";
+import pendantOne from "../../assets/pendant_1.png";
+
+import Card from "../Card";
+
+const cardData = [
+  {
+    classes: "justify-content-xxl-start",
+    src: pendantOne,
+    title: "Unique",
+    details:
+      "Every design has a story to tell, H&H Jewels spends a lot of time in ensuring that every jewelry from us  makes you stand out from the crowd.",
+  },
+  {
+    classes: "justify-content-xxl-center align-items-xxl-end",
+    src: ringsEarrings,
+    title: "Quality",
+    details:
+      "Our jewelries are made of high quality and durable materials that have the ability to retain its shine and luster value for a long time.",
+  },
+  {
+    classes: "justify-content-xxl-end",
+    src: earringsTwo,
+    title: "Comfort",
+    details:
+      "H&H Jewels always put the customer first, that is why our designs are carefully selected to be ultra comfortable and light weight.",
+  },
+];
+
+const AboutUs = () => {
+  return (
+    <section id="about-us">
+      <div className="container">
+        <h2 className="mt-100 mb-5 ff-serif fw-bold fs-sm-5 fs-md-5 fs-xl-6 text-center">
+          About us
+        </h2>
+        <div className="row g-5 ">
+          <div className="col-xxl d-flex justify-content-center justify-content-xxl-start">
+            <figure className="w-182 h-281 w-md-270 h-md-400 w-xl-344 h-xl-476 px-2 py-3 px-md-3 py-md-4 px-xl-17 py-xl-27 bord custom-brad-1">
+              <img
+                className="w-100 h-100 custom-brad-1"
+                src={necklaceTwo}
+                alt="girl wearing a necklace"
+              ></img>
+            </figure>
+          </div>
+          <div className="col-xxl d-flex justify-content-center align-items-center">
+            <div className="custom-col d-flex justify-content-center align-items-end px-5 py-4">
+              <p className="fs-sm-2 fs-md-2 fs-xl-2 text-center fw-light lh-1 m-0">
+                H&H Jewels is a brand established by Husna, with a craving for
+                polished jewelries, she is dedicated to bringing you the perfect
+                piece that will bring out the best in every woman’s features and
+                personality. The brand is proficient and your ultimate source
+                for ordering fine jewelry from the comfort of your home.
+              </p>
+            </div>
+          </div>
+          <div className="col-xxl d-flex justify-content-center justify-content-xxl-end">
+            <figure className="w-182 h-281 w-md-270 h-md-400 w-xl-344 h-xl-476 px-2 py-3 px-md-3 py-md-4 px-xl-17 py-xl-27 bord custom-brad-2 ">
+              <img
+                className="w-100 h-100 of-cover custom-brad-2"
+                src={necklaceOne}
+                alt="girl wearing a necklace"
+              ></img>
+            </figure>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <h2 className="mt-50 mt-md-100  ff-serif fw-bold fs-sm-5 fs-md-5 fs-xl-6 text-center">
+          Why Choose Us
+        </h2>
+        <div className="row g-5 m-0 h-xxl-684 justify-content-between">
+          {cardData.length &&
+            cardData.map((item, index) => (
+              <div
+                key={index}
+                className={`col-xxl d-flex justify-content-center ${item.classes}`}
+              >
+                <Card
+                  src={item.src}
+                  title={item.title}
+                  details={item.details}
+                />
+              </div>
+            ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs;
---
+++ src/components/layout/Category.js
@@ -0,0 +1,64 @@
+import bangles from "../../assets/bangles.png";
+import ringOne from "../../assets/ring_1.png";
+import earringsOne from "../../assets/earrings_1.png";
+import fullSet from "../../assets/full_set.png";
+import model from "../../assets/model.png";
+
+import ProductCardGroup from "../ProductCardGroup";
+
+const cardDataOne = [
+  {
+    classes: "justify-content-xxl-start mb-3 mb-md-5",
+    src: ringOne,
+    title: "Ring",
+  },
+  {
+    classes: "justify-content-xxl-start align-items-xxl-end",
+    src: bangles,
+    title: "Bracelet",
+  },
+];
+
+const cardDataTwo = [
+  {
+    classes: "justify-content-xxl-end mb-3 mb-md-5",
+    src: earringsOne,
+    title: "Earrings",
+  },
+  {
+    classes: "justify-content-xxl-end align-items-xxl-end",
+    src: fullSet,
+    title: "Necklace",
+  },
+];
+
+const Category = () => {
+  return (
+    <section id="category">
+      <div className="container">
+        <h2 className="mt-50 mt-md-100 mb-5 ff-serif fw-bold fs-sm-5 fs-md-5 fs-xl-6 text-center">
+          Category
+        </h2>
+        <div className="row g-3 g-md-5 h-xxl-776">
+          <div className="col-xxl">
+            <ProductCardGroup cardData={cardDataOne} />
+          </div>
+          <div className="col-xxl d-flex justify-content-center justify-content-xxl-center">
+            <figure className="w-182 h-281 w-md-270 h-md-400 w-xxl-520 h-xxl-762 bord-l custom-brad-1">
+              <img
+                className="w-100 h-100 custom-brad-1"
+                src={model}
+                alt="girl wearing a necklace"
+              ></img>
+            </figure>
+          </div>
+          <div className="col-xxl">
+            <ProductCardGroup cardData={cardDataTwo} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Category;
