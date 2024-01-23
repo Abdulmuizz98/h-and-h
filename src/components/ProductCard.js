c662241e3587168fdc2201b5cd4be1a0cf0ed617
Abdulmuizz98 <hamzatabdulmuizz98@gmail.com>
2024-01-23 11:35:56 +0100

    fix: category dimensions

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,6 +1,6 @@
 const ProductCard = ({ src, title }) => {
   return (
-    <figure className="">
+    <figure className="w-100">
       <div className="text-center d-flex align-items-center justify-content-center pt-3 pt-md-4 pt-xl-5 w-100 h-107 h-md-150 bord custom-brad-1">
         <h3 className="fs-sm-4 fs-md-3 fs-xl-5 fw-bold ff-serif">{title}</h3>
       </div>
